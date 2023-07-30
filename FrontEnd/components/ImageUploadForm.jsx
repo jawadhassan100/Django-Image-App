ddb87ca6d4feeb789efced8f2a410af180bcdf9c
Jawad Hassan <jawadhassankhan2001@gmail.com>
2023-07-30 18:33:00 +0500

    image delete btn fixed

--- FrontEnd/components/ImageUploadForm.jsx
+++ FrontEnd/components/ImageUploadForm.jsx
@@ -11,7 +11,7 @@ const MyPost = () => {
   const router = useRouter()
   const deleteImage = async (id) => {
     try {
-        await axios.delete(`http://localhost:3000/api/images//api/images/${id}/`)
+        await axios.delete(`http://127.0.0.1:8000/api/images/${id}/`)
         console.log(`image with id ${id} deleted`)
     } catch (error) {
         console.log(`image with id ${id} is not deleted`)
@@ -42,22 +42,20 @@ const toHome = () => {
 
   return (
     <div className="bg-gradient-to-br from-blue-900 to-purple-900 min-h-screen ">
-        
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 flex items-center justify-center ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 items-center justify-center ">
           {data.map((image) => (
              <div
               key={image.id}
               className="flex flex-col items-center bg-gradient-to-br from-blue-400 to-purple-500 rounded-lg shadow p-4  mt-6"
             >
-              <img
-                src={image.image}
-                alt="Uploaded"
-                className="w-full h-full object-cover  rounded-lg mb-4 "
-                
-              />
+            <img
+              src={image.image}
+              alt="Uploaded"
+              className="w-full object-contain rounded-xl mb-4"
+            />
               <button
                 onClick={() => deleteHandler(image.id)}
-                className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-lg"
+                className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-lg "
               >
                 Delete Image
               </button>
