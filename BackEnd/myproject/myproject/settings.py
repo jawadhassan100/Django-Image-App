29a6eba5de1ff27daede706b9eb9406f9ec47407
jawadhassan100 <jawadhassankhan2001@gmail.com>
2024-08-28 15:42:49 +0500

    updated

--- BackEnd/myproject/myproject/settings.py
+++ BackEnd/myproject/myproject/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-gyv)r+h5m)hm_o*u!g++5mf2ngcfl)s@&iruc^agy(t4t2xb$l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -133,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
